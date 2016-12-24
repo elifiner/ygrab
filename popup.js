2870f447ffd61e8d7fa7c8f400ef3449e6aebefe
Eli Finer <eli.finer@gmail.com>
2016-12-24 10:52:12 -0800

    added jquery and refactored

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
-function scrapeCurrentTab() {
-    chrome.tabs.getSelected(null, function(tab){
-        chrome.tabs.executeScript(tab.id, {file: 'jquery.js'}, function(response) { 
-            chrome.tabs.executeScript(tab.id, {file: 'scrape.js'}, function(response) { 
-                chrome.tabs.executeScript(tab.id, {code: 'scrape()'}, function() {
+$(function () {
+    function scrapeCurrentTab() {
+        chrome.tabs.getSelected(null, function(tab){
+            chrome.tabs.executeScript(tab.id, {file: 'jquery.js'}, function(response) { 
+                chrome.tabs.executeScript(tab.id, {file: 'scrape.js'}, function(response) { 
+                    chrome.tabs.executeScript(tab.id, {code: 'scrape()'}, function() {
+                    });
                 });
             });
         });
-    });
-}
+    }
 
-// handle click on popup link
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('scrape').addEventListener('click', scrapeCurrentTab);
+    $('#title').text('Title!');
+    $('#scrape').click(scrapeCurrentTab);
 });
